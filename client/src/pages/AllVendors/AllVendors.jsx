45c198e4420382a834df0f6ceba929012347e7eb
Md-Huzaifa-Islam <HuzaifaIslamRakib@gmail.com>
2025-05-24 16:37:35 +0600

    updated the navbar

--- client/src/pages/AllVendors/AllVendors.jsx
+++ client/src/pages/AllVendors/AllVendors.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 export default function AllVendors() {
   return (
-    <div className=" bg-base-200 pt-24 ">
+    <div className=" bg-base-200 pt-24 mb-10 ">
       <div className="max-w-6xl mx-auto">
         <h1 className="text-4xl font-bold text-center mb-2 text-primary">
           Explore Campus Vendors
