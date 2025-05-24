1ad37218abe3a230ba8eb2946246c7fd8c7ba1d1
Md-Huzaifa-Islam <HuzaifaIslamRakib@gmail.com>
2025-05-24 11:13:09 +0600

    resolved conflict

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Login from "./components/login.jsx";
-import SignUp from "./components/SignUp.jsx";
-
-createRoot(document.getElementById("root")).render(<StrictMode></StrictMode>);
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/Route.jsx";
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
